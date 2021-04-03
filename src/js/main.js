a100a28671ac4917ae3b809cdb2963ed0c9db57a
Vitalii Sosluik <soslukven@gmail.com>
2021-04-03 16:02:34 +0300

    Realizing timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,12 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/form';
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener('DOMContentLoaded', () => {
 
   let modalState = {};
+  let deadline = '2021-04-05';
 
   changeModalState(modalState);
   modals();
@@ -25,5 +27,6 @@ window.addEventListener('DOMContentLoaded', () => {
     'inline-block'
   );
   forms(modalState);
+  timer('.container1', deadline);
 
 });
